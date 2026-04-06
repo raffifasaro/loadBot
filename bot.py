17eabceb576179c8ebc08c3a9e9dc2785b744c34
raffifasaro <raffifasaro@gmail.com>
2026-04-06 17:54:48 +0200

    add simple slash command functionality

--- bot.py
+++ bot.py
@@ -1,8 +1,13 @@
 import discord
 import asyncio
 import config
+from discord import app_commands
 
 class VideoBot(discord.Client):
+
+    def __init__(self, intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
 
     async def on_ready(self):
         print(f"Logged in as {self.user}")
@@ -14,6 +19,14 @@ class VideoBot(discord.Client):
         if message.content == "!ping":
             await message.channel.send("pong")
 
+    async def setup_hook(self):
+        @self.tree.command(name="download", description="Download media from link")
+        async def download(interaction: discord.Interaction):
+            await interaction.response.send_message("Downloading media")
+
+        await self.tree.sync()
+
+        
 
 async def main():
     intents = discord.Intents.default()
